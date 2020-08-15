971311fdcd097e85234508d4cb52c3769486b93f
GuilhermeVilatoro <vilatorog@gmail.com>
2020-08-15 10:27:14 -0300

    Ajuste na jdk.

--- src/Main.java
+++ src/Main.java
@@ -9,23 +9,22 @@ public class Main {
 		System.out.println(y);
 		System.out.println(x);		
 		
-		System.out.printf("%.2f%n", x);
+		//System.out.printf("%.2f%n", x);
 		
-		System.out.printf("%.4f\n", x);
+		//System.out.printf("%.4f\n", x);
 		
-		System.out.printf(Locale.US, "%.2f\n", x);
+	//	System.out.printf(Locale.US, "%.2f\n", x);
 		
 		System.out.println("Olá mundo!");
 		System.out.println("Bom dia!");
 		
 		System.out.println("RESULTADO = " + x + " METROS");
-		System.out.printf("Resultado = %.2f metros%n", x, x);
+		//System.out.printf("Resultado = %.2f metros%n", x, x);
 		
 		int idade = 30;
 		double salario = 4000.0;
 		String nome = "João";
 		
-		System.out.printf("%s tem %d anos e ganha R$%.2f%n", nome, idade, salario);
-
+		//System.out.printf("%s tem %d anos e ganha R$%.2f%n", nome, idade, salario);	
 	}
 }
